4bd3f1106233afedb4dddc0cf4d0604115347d1d
nasonfish <nasonfish@gmail.com>
2014-03-31 22:07:13 -0600

    help: document help command

--- commands/help.py
+++ commands/help.py
@@ -4,7 +4,13 @@ from utils import hook, HookFlags
 from re import compile
 
 flags = HookFlags(r=('regex', True))
-@hook.command('help', args_amt=lambda x: 0 <= len(x) <= 2, flags=flags, return_json=False)
+@hook.command('help', args_amt=lambda x: 0 <= len(x) <= 2, flags=flags, return_json=False, doc=("Fetch information regarding a certain command",
+                                                                                                "`help' will, without any arguments, list all commands and a brief idea of what they do.",
+                                                                                                "Once supplied with a command, `help' will return all documentation regarding that command. Any command listed in `help' will return documentation"
+                                                                                                "Flags: (only when no command is supplied)",
+                                                                                                "\t-r, --regex <regex>: filter results by a provided regular expression.",
+                                                                                                "Usage:",
+                                                                                                "`help [-r <regex>|<my command>]'"))
 def help(args, flags):
     # this returns the string we want to print out as help.
     command = ' '.join(args)
