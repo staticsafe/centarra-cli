1738011f704472d294b84f5f585f75a7e4bf8010
William Pitcock <nenolod@dereferenced.org>
2014-04-11 02:57:03 -0500

    vps console: more work on the event loop, basically working now except for control code filtering

--- commands/vpsconsole.py
+++ commands/vpsconsole.py
@@ -1,6 +1,13 @@
 import gevent
 import sys
+import termios
+import tty
+import atexit
+import fcntl
+import os
+
 from gevent.event import Event
+from gevent.socket import wait_read
 
 from wssh.client import StdioPipedWebSocketClient
 from wssh.common import StdioPipedWebSocketHelper
@@ -12,12 +19,35 @@ from utils import hook, HookFlags, JsonResponse
 from libs import centarra, substitutes, sub
 from utils.domain import is_valid_host
 
+def enable_echo(fd, enabled):
+    if enabled:
+        tty.setcbreak(fd, termios.TCSANOW)
+    else:
+        tty.setraw(fd, termios.TCSANOW)
+
 class VPSArgsObject:
     def __init__(self):
         self.text_mode = 'auto'
         self.verbosity = 0
         self.new_lines = False
         self.quit_on_eof = True
+
+class GEventFD:
+    def __init__(self, fd):
+        self.fd = fd
+        fcntl.fcntl(self.fd, fcntl.F_SETFL, os.O_NONBLOCK)
+        self.w_pending = ""
+
+    def read(self, total_to_read):
+        wait_read(self.fd)
+        return os.read(self.fd, total_to_read)
+
+    def write(self, data):
+        self.w_pending += data
+        while self.w_pending:
+            wait_write(self.fd)
+            written = os.write(self.fd, self.w_pending)
+            self.w_pending = self.w_pending[written:]
 
 class VPSConsoleIOHelper(StdioPipedWebSocketHelper):
     """
@@ -27,6 +57,11 @@ class VPSConsoleIOHelper(StdioPipedWebSocketHelper):
 
     def __init__(self, shutdown_cond, opts):
         StdioPipedWebSocketHelper.__init__(self, shutdown_cond, opts)
+        self.setup_echo()
+
+    def setup_echo(self):
+        enable_echo(sys.stdin.fileno(), False)
+        atexit.register(enable_echo, sys.stdin.fileno(), True)
 
     def received_message(self, websocket, m):
         chunk_size = 16
@@ -46,6 +81,20 @@ class VPSConsoleIOHelper(StdioPipedWebSocketHelper):
                 except IOError:
                     flushed = False
 
+    def opened(self, websocket):
+        def connect_stdin():
+            stdin = GEventFD(sys.stdin.fileno())
+            while True:
+                try:
+                    buf = stdin.read(1)
+                    if buf[0] == chr(4):
+                        self.shutdown_cond.set()
+                        break
+                    websocket.send(buf, False)
+                except:
+                    continue
+        gevent.spawn(connect_stdin)
+
 class VPSConsoleClient(StdioPipedWebSocketClient):
     def __init__(self, uri):
         WebSocketClient.__init__(self, uri)
@@ -61,9 +110,9 @@ def vps_console(args, flags):
     if not service:
         return 'Cannot find WSS-API endpoint'
     client = VPSConsoleClient(service['wss_console_uri'])
-    print('[Console attached, press ^D to exit.]')
+    print('[Console attached, press ^D to exit.]\r\n')
     try:
         client.connect_and_wait()
     except (IOError, HandshakeError), e:
         print >> sys.stderr, e
-    return '[Console detached.]'
+    return '[Console detached.]\r'
